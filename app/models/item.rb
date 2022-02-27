524ca3aac877b98f634bf217dbf162fd1fa3a574
Ayumu Kojima <youki.6230@gmail.com>
2022-02-27 22:23:37 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :send_day
 end
