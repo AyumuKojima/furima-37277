799e214ca2004b378dacab42fdec98e0b47c8d4b
Ayumu Kojima <youki.6230@gmail.com>
2022-03-01 22:19:53 +0900

    OrderAddressクラスの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,13 +9,12 @@ class OrderAddress
     validates :city
     validates :house_number
     validates :phone_number, numericality: {only_integer: true}, length: {minimum: 10, maximum: 11}
-    validates :order_id
     validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
